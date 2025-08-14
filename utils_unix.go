1db72fb9a49be71555a615682286970ad50abd9b
Thomas Elsgaard <80691187+elsgaard@users.noreply.github.com>
2025-08-14 12:50:39 +0200

    Add files via upload

---
+++ utils_unix.go
@@ -0,0 +1,13 @@
+//go:build darwin || dragonfly || freebsd || (linux && !appengine) || netbsd || openbsd || solaris
+// +build darwin dragonfly freebsd linux,!appengine netbsd openbsd solaris
+
+package ishell
+
+import (
+	"github.com/abiosoft/readline"
+)
+
+func clearScreen(s *Shell) error {
+	_, err := readline.ClearScreen(s.writer)
+	return err
+}
